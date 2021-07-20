e9c628fd303a2238cdf22baa789b502376adc4ef
kathu@08 <sivalingamk.19@uom.lk>
2021-07-20 17:26:59 +0530

    first commit

---
+++ planning.js
@@ -0,0 +1,22 @@
+// Book my show planning
+
+// we need a reusable carousel
+//      hero carousel
+//      common carousel
+//      cast and crew small carousel
+
+// Pages
+//  home page
+//  movie page
+//  categories page
+//  auth page
+//  razorpay payment gateway
+
+// Plans --> how are we going to build??? 
+// layouts
+// routes
+// pages
+// components
+
+// HOC 
+// Higher Order Components
